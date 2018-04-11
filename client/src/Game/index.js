5c494498579552d4fb9675c8833b3723e13d6103
Gustavo Giserman <gugiserman@gmail.com>
2018-04-11 04:39:14 -0300

    Implement players drawing placeholder and initial game drawing

--- client/src/Game/index.js
+++ client/src/Game/index.js
@@ -1,12 +1,17 @@
+const rowCount = 15
+const columnCount = 15
+
 class Game {
-  constructor(canvas, io, playerCount) {
+  constructor(canvas, io, playerCount = 2) {
     this.io = io
     this.canvas = canvas
     this.context = this.canvas.getContext('2d')
+    this.tileDimensions = [(this.canvas.width / rowCount), (this.canvas.height / columnCount)]
     this.playerCount = playerCount
     this.matchId = Math.random()
 
     this.renderMap()
+    this.renderPlayers()
     this.renderContainer()
   }
 
@@ -16,26 +21,55 @@ class Game {
   }
 
   renderMap() {
-    const tileDimensions = [(this.canvas.width / 20), (this.canvas.height / 15)]
     const rowTemplate = []
 
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < rowCount; i++) {
       rowTemplate.push(0)
     }
 
     const tileMap = []
 
-    for (let i = 0; i < 15; i++) {
+    for (let i = 0; i < columnCount; i++) {
       tileMap.push(rowTemplate.slice())
     }
 
     tileMap.forEach((row, y) => {
       row.forEach((tile, x) => {
         this.context.rect(
-          x * tileDimensions[0], y * tileDimensions[1],
-          tileDimensions[0], tileDimensions[1]
+          x * this.tileDimensions[0], y * this.tileDimensions[1],
+          this.tileDimensions[0], this.tileDimensions[1]
         )
       })
+    })
+  }
+
+  renderPlayers() {
+    const middleRow = Math.ceil(rowCount / 2)
+
+    const firstPlayer = {
+      row: 3,
+      column: middleRow,
+      color: '#0000b2',
+    }
+
+    const secondPlayer = {
+      row: (rowCount - 2),
+      column: middleRow,
+      color: '#cc0000',
+    }
+
+    const players = [firstPlayer, secondPlayer]
+
+    players.forEach(player => {
+      const x = (player.row * this.tileDimensions[0]) - (this.tileDimensions[0] / 2)
+      const y = Math.ceil(player.column * this.tileDimensions[1]) - (this.tileDimensions[1] / 2)
+      this.context.fillStyle = player.color
+      this.context.strokeStyle = '#333333'
+      this.context.lineWidth = 1
+      this.context.beginPath()
+      this.context.arc(x, y, (this.tileDimensions[1] / 2), 0, (2 * Math.PI))
+      this.context.fill()
+      this.context.stroke()
     })
   }
 }
